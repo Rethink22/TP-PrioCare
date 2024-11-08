518c7ad43bd1a7f22a016dc2a58cbfe7d6717baa
JStockschen <jcjstockschen@student.che.nl>
2024-11-08 19:23:29 +0100

    Added Loicences

--- PrioCareApplication.py
+++ PrioCareApplication.py
@@ -1,3 +1,20 @@
+#    PrioCare: for analyzing annoying patient's complaints, and giving them an urgency rating based on their issue.
+#    Copyright (C) 2024  Rethink22, MNellestijn, JStockschen, Johan Huizinga, Sam Zwols
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 # general libraries
 from datetime import datetime
 import time
